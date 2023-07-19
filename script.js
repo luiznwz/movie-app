39a8dadb6f95f8cb8b0405c59071628365b340da
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2023-07-18 21:30:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -99,3 +99,50 @@ var nextPage = 2;
 var prevPage = 3;
 var lastUrl = '';
 var totalPages = 100;
+
+var selectedGenre = [] 
+setGenre();
+function setGenre() {
+  tagsEl.innerHTML='';
+  genres.forEach(genre => {
+    const t = document.createElement('div');
+    t.classList.add('tag');
+    t.id=genre.id;
+    t.innerText = genre.name;
+    t.addEventListener('click', () => {
+      if(selectedGenre.length == 0){
+        selectedGenre.push(genre.id);
+      }else{
+        if(selectedGenre.includes(genre.id)){
+          selectedGenre.forEach((id, idx) => {
+            if(id == genre.id){
+              selectedGenre.splice(idx, 1);
+            }
+          })
+        }else{
+          selectedGenre.push(genre.id);
+        }
+      }
+      console.log(selectedGenre)
+      getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+      highlightSelection()
+  })
+  tagsEl.append(t);
+    })
+  }
+
+  function highlightSelection() {
+    const tags = document.querySelector('.tag');
+    tags.forEach(tag => {
+      tag.classList.remove('highlight')
+    })
+    clearBtn()
+    if(selectedGenre.length !=0){
+      selectedGenre.forEach(id => {
+        const highlightedTag = document.getElementById(id);
+        highlightedTag.classList.add('highlight');
+      })
+    }
+    }
+
+    
